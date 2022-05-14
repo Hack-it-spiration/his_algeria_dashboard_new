b2c0cdf19c3cc186b280e7ce89ca3a77cc5fcc8a
unknown <is_metidji@esi.dz>
2022-05-14 10:11:44 +0100

    removing calendar and auth ( except profile )

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -2,18 +2,12 @@ import { combineReducers } from "redux"
 
 // Front
 import Layout from "./layout/reducer"
-
-// Authentication
-// import Login from "./auth/login/reducer"
-// import Account from "./auth/register/reducer"
-// import ForgetPassword from "./auth/forgetpwd/reducer"
 import Profile from "./auth/profile/reducer"
 
 //E-commerce
 import ecommerce from "./e-commerce/reducer"
 
 //Calendar
-import calendar from "./calendar/reducer"
 
 //chat
 import chat from "./chat/reducer"
--- src/store/sagas.js
+++ src/store/sagas.js
@@ -1,13 +1,8 @@
 import { all, fork } from "redux-saga/effects"
 
-//public
-import AccountSaga from "./auth/register/saga"
-import AuthSaga from "./auth/login/saga"
-import ForgetSaga from "./auth/forgetpwd/saga"
 import ProfileSaga from "./auth/profile/saga"
 import LayoutSaga from "./layout/saga"
 import ecommerceSaga from "./e-commerce/saga"
-import calendarSaga from "./calendar/saga"
 import chatSaga from "./chat/saga"
 import cryptoSaga from "./crypto/saga"
 import invoiceSaga from "./invoices/saga"
@@ -21,9 +16,6 @@ import dashboardSaasSaga from "./dashboard-saas/saga";
 export default function* rootSaga() {
   yield all([
     //public
-    fork(AccountSaga),
-    fork(AuthSaga),
-    fork(ForgetSaga),
     fork(ProfileSaga),
     fork(LayoutSaga),
     fork(ecommerceSaga),
